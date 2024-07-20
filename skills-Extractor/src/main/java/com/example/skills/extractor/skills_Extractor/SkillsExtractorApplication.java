310c581c099b59e45c52653542982a514f2a01f0
Pradeep DEIVAMANI <133005962+PradeepDEIVAMANI@users.noreply.github.com>
2024-07-21 00:41:31 +0200

    First Commit

---
+++ skills-Extractor/src/main/java/com/example/skills/extractor/skills_Extractor/SkillsExtractorApplication.java
@@ -0,0 +1,13 @@
+package com.example.skills.extractor.skills_Extractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillsExtractorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillsExtractorApplication.class, args);
+	}
+
+}
---
+++ skills-Extractor/src/test/java/com/example/skills/extractor/skills_Extractor/SkillsExtractorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skills.extractor.skills_Extractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillsExtractorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
